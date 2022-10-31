ba2107e6173216e91a07d560492b0d4fb322f41d
Guilherme Araujo <guilherme1984.araujo@gmail.com>
2022-10-31 18:55:31 -0300

    1 commit dever de DS1 adm condominio

---
+++ Condominio/src/Condominio/Condominio.java
@@ -0,0 +1,10 @@
+package Condominio;
+
+public class Condominio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Condominio/src/Condominio/CustosCondominiais.java
@@ -0,0 +1,10 @@
+package Condominio;
+
+public class CustosCondominiais {
+	private float contaEnergia;
+	private float contaAgua;
+	private float salarioLimpeza;
+	private int mesPintura;
+	private int mesServiços;
+	private float valorTotalMensalPagar;
+}
