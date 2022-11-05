d43584f2541a3e5da2200281269adfe77a305a2a
Guilherme Araujo <guilherme1984.araujo@gmail.com>
2022-11-04 23:38:37 -0300

    Projeto vacina pronto para entregar para pró

--- ProjetoVacina/src/ProjetoVacina/ProjetoVacina.java
+++ ProjetoVacina/src/ProjetoVacina/ProjetoVacina.java
@@ -3,8 +3,40 @@ package ProjetoVacina;
 public class ProjetoVacina {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Virus virus = new Virus();
+		Virus virus1 = new Virus();
+		Virus virus2 = new Virus();
+		Virus virus3 = new Virus();
 
+		// criando o objeto0
+		virus.setNomeVirus("Sarampo");
+		virus.setVacina(true);
+		virus.setTaxaMortalidade(0.5f);
+
+		// criando o objeto1
+		virus1.setNomeVirus("pagodeiro");
+		virus1.setVacina(false);
+		virus1.setTaxaMortalidade(0.5f);
+
+		// criando o objeto2
+		virus2.setNomeVirus("sofrencia");
+		virus2.setVacina(true);
+		virus2.setTaxaMortalidade(0.1f);
+
+		// criando o objeto3
+		virus3.setNomeVirus("doido de pedra");
+		virus3.setVacina(true);
+		virus3.setTaxaMortalidade(0.8f);
+
+		System.out.println("====================");
+		System.out.println(virus);
+		System.out.println("====================");
+		System.out.println(virus1);
+		System.out.println("====================");
+		System.out.println(virus2);
+		System.out.println("====================");
+		System.out.println(virus3);
+		
 	}
 
 }
--- ProjetoVacina/src/ProjetoVacina/Virus.java
+++ ProjetoVacina/src/ProjetoVacina/Virus.java
@@ -4,4 +4,51 @@ public class Virus {
 	private String nomeVirus;
 	private float taxaMortalidade;
 	private boolean vacina;
+	
+	
+	public String getNomeVirus() {
+		return nomeVirus;
+	}
+	public void setNomeVirus(String nomeVirus) {
+		this.nomeVirus = nomeVirus;
+	}
+	public float getTaxaMortalidade() {
+		return taxaMortalidade;
+	}
+	public void setTaxaMortalidade(float taxaMortalidade) {
+		this.taxaMortalidade = taxaMortalidade;
+	}
+	public boolean isVacina() {
+		return vacina;
+	}
+	public void setVacina(boolean vacina) {
+		this.vacina = vacina;
+	}
+	
+	public String toString() {
+		int gravidade;
+		if (!this.isVacina()&& this.getTaxaMortalidade() ==0.5f) {
+			gravidade = 5;
+		}
+		else if(!this.isVacina()&& this.getTaxaMortalidade() <0.5f ){
+			gravidade = 4;
+		}
+		else if(this.isVacina()&& this.getTaxaMortalidade() >0.5f) {
+			gravidade = 3;
+		}
+		else if(this.isVacina()&& this.getTaxaMortalidade() >=0.2f && 
+				this.getTaxaMortalidade() <=0.4) {
+			gravidade = 2;
+		}
+		else {
+			gravidade = 1 ;
+		}
+		
+			return "O nome do Virus e: \n "
+					+ this.getNomeVirus()
+					+"\nA gravidade da situacao e: "
+					+"\n Gravidade nivel: "
+					+ gravidade ;
+	}
+	
 }
