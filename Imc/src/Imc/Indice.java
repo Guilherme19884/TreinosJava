e4fb6b005bfaae6f8519928f38f0a6246b756c39
Guilherme Araujo <guilherme1984.araujo@gmail.com>
2022-11-08 09:37:09 -0300

    criando Imc

---
+++ Imc/src/Imc/Indice.java
@@ -0,0 +1,14 @@
+package Imc;
+
+public class Indice {
+
+	public static void main(String[] args) {
+		Imc imc = new Imc();
+		
+		imc.setNome("Guilherme");
+		imc.setAltura(1.67f);
+		imc.setPeso(67f);
+
+	}
+
+}
