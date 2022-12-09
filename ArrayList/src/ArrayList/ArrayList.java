d8b70ab1a754b009a3b47f00e50d1e3e496b98e7
Guilherme Araujo <guilherme1984.araujo@gmail.com>
2022-12-09 10:42:26 -0300

    concluído os comandos de List para treino

--- ArrayList/src/ArrayList/ArrayList.java
+++ ArrayList/src/ArrayList/ArrayList.java
@@ -1,10 +1,45 @@
 package ArrayList;
 
+import java.util.List;
+
 public class ArrayList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List <String> listaPessoas = new java.util.ArrayList<>();
+		
+		listaPessoas.add("Maria");
+		listaPessoas.add("Alex");
+		listaPessoas.add("Bob");
+		listaPessoas.add("Anna");
+		listaPessoas.add("Marco");
+		
+		//para saber quantos elementos tem na lista.
+		System.out.println(listaPessoas.size());
+		
+		System.out.println("-----------------");
+		// para mostrar todos na lista 
+		for(String p : listaPessoas) {
+			// quando é um List importante colocar o elemento que está dentro do símbolo <>
+			System.out.println(p);
+		}
+		System.out.println("-----------------");
+		// para remover pessoas da lista com determinada incial
+		
+		listaPessoas.removeIf(p -> p.charAt(0) == 'M');
+		for (String p : listaPessoas) {
+			System.out.println(p);
+		}
+		System.out.println("-----------------");
+		// serve para mostrar o numero que é equivalente  para ao nome
+		System.out.println("Index de Bob: " + listaPessoas.indexOf("Bob"));
+		// observe que o de Marco não existe porque foi retirado antes e o resultado 
+		// dá negativo: -1
+		System.out.println("Index de Marco: " + listaPessoas.indexOf("Marco"));
+		
+		System.out.println("-----------------");
+		String nome = listaPessoas.stream().filter(p-> p.charAt(0) == 'A').
+				findFirst().orElse(null);
+		System.out.println(nome);
 	}
 
 }
