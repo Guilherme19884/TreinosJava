2d330fdad568a92f164b714dfa3afac456dcf6bf
Guilherme Santos Araújo <guilherme1984.araujo@gmail.com>
2023-01-13 14:34:03 -0300

    ex02 POO, DioPro, continuação da classe carro

--- DioExPooCarro/src/DioExPooCarro/Carro.java
+++ DioExPooCarro/src/DioExPooCarro/Carro.java
@@ -7,4 +7,43 @@ public class Carro {
 	private float capacidadeTanque;
 	
 	
+	public Carro(int portas, String cor, String modelo, float capacidadeTanque) {
+		super();
+		this.portas = portas;
+		this.cor = cor;
+		this.modelo = modelo;
+		this.capacidadeTanque = capacidadeTanque;
+	}
+	
+	public int getPortas() {
+		return portas;
+	}
+	public void setPortas(int portas) {
+		this.portas = portas;
+	}
+	public String getCor() {
+		return cor;
+	}
+	
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public float getCapacidadeTanque() {
+		return capacidadeTanque;
+	}
+	public void setCapacidadeTanque(float capacidadeTanque) {
+		this.capacidadeTanque = capacidadeTanque;
+	}
+	
+	public float valorTotaltanque(float valorCombustivel) {
+		return capacidadeTanque *= valorCombustivel;
+	}
+
+	
 }
